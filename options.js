7a80310040770ed7051f114e1526eb3d0221d5b6
Akito Tabira <mythosil@yahoo.co.jp>
2018-12-30 21:02:33 +0900

    configurable on options ui

---
+++ options.js
@@ -0,0 +1,32 @@
+var browser = browser || chrome;
+
+const defaultBlacklist = [
+  'code.i-harness.com',
+  'sejuku.net',
+];
+
+function saveOptions(e) {
+  e.preventDefault();
+  const s = document.querySelector('textarea').value;
+  const blacklist = s.split(/\r?\n/);
+  browser.storage.local.set({
+    blacklist: blacklist
+  });
+}
+
+function restoreOptions(e) {
+  function renderToTextarea(blacklist) {
+    if (!blacklist) {
+      blacklist = defaultBlacklist;
+    }
+    const s = blacklist.join("\r\n");
+    document.querySelector('textarea').textContent = s;
+  }
+
+  let getting = browser.storage.local.get('blacklist', result => {
+    renderToTextarea(result.blacklist);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
--- rasr.js
+++ rasr.js
@@ -1,11 +1,27 @@
 var browser = browser || chrome;
 
-const blacklist = [
+const defaultBlacklist = [
   'code.i-harness.com',
   'sejuku.net/blog',
 ];
 
-function inBlacklist(url) {
+const iconUrl = browser.extension.getURL("icons/icon-32.png");
+
+async function getBlacklist() {
+  const p = new Promise((resolve, reject) => {
+    browser.storage.local.get('blacklist', result => {
+      if (result.blacklist) {
+        resolve(result.blacklist);
+      } else {
+        resolve(defaultBlacklist);
+      }
+    });
+  });
+  return await p;
+}
+
+async function inBlacklist(url) {
+  const blacklist = await getBlacklist();
   for (let b of blacklist) {
     if (url.includes(b)) {
       return true;
@@ -14,12 +30,8 @@ function inBlacklist(url) {
   return false;
 }
 
-const iconUrl = browser.extension.getURL("icons/icon-32.png");
-
-const search = document.querySelector('#search');
-const gs = search.querySelectorAll('.g');
-gs.forEach(g => {
-  const r = g.querySelector('.r');
+async function filterAnnoying(gElem) {
+  const r = gElem.querySelector('.r');
   if (!r) {
     return;
   }
@@ -28,8 +40,12 @@ gs.forEach(g => {
     return;
   }
   const url = a.href;
-  if (inBlacklist(url)) {
-    // annoying!!!
-    g.innerHTML = `<p><img src="${iconUrl}" width="24" height="24" style="vertical-align: middle"> ${url}</p>`;
+  const annoying = await inBlacklist(url);
+  if (annoying) {
+    gElem.innerHTML = `<p><img src="${iconUrl}" width="24" height="24" style="vertical-align: middle"> ${url}</p>`;
   }
-});
+}
+
+const search = document.querySelector('#search');
+const gs = search.querySelectorAll('.g');
+gs.forEach(g => filterAnnoying(g));
